68e8d87687a9768f3dc66f80c5b9cdcf13a6947c
shefben <shefben@gmail.com>
2025-10-18 19:35:41 -0500

    Enhance namespace tree item appearance overrides

--- src/NameColorProvider.cpp
+++ src/NameColorProvider.cpp
@@ -11,15 +11,37 @@ NameColorProvider& NameColorProvider::Instance() {
 }
 
 bool NameColorProvider::TryGetColorForPath(const std::wstring& path, COLORREF* color) const {
-    if (path.empty()) {
+    const ItemAppearance appearance = GetAppearanceForPath(path);
+    if (!appearance.textColor.has_value()) {
         return false;
     }
+    if (color) {
+        *color = *appearance.textColor;
+    }
+    return true;
+}
 
-    if (FileColorOverrides::Instance().TryGetColor(path, color)) {
-        return true;
+NameColorProvider::ItemAppearance NameColorProvider::GetAppearanceForPath(
+        const std::wstring& path) const {
+    ItemAppearance appearance;
+    if (path.empty()) {
+        return appearance;
     }
 
-    return TagStore::Instance().TryGetColorForPath(path, color);
+    COLORREF overrideColor = 0;
+    if (FileColorOverrides::Instance().TryGetColor(path, &overrideColor)) {
+        appearance.textColor = overrideColor;
+        appearance.applyWhenHot = true;
+        appearance.applyWhenSelected = true;
+        return appearance;
+    }
+
+    COLORREF tagColor = 0;
+    if (TagStore::Instance().TryGetColorForPath(path, &tagColor)) {
+        appearance.textColor = tagColor;
+    }
+
+    return appearance;
 }
 
 bool NameColorProvider::TryGetColorAndTags(const std::wstring& path, COLORREF* color,
@@ -32,9 +54,6 @@ bool NameColorProvider::TryGetColorAndTags(const std::wstring& path, COLORREF* c
         return false;
     }
 
-    COLORREF overrideColor = 0;
-    const bool hasOverride = FileColorOverrides::Instance().TryGetColor(path, &overrideColor);
-
     std::vector<std::wstring> resolvedTags;
     COLORREF tagColor = 0;
     const bool hasTagColor = TagStore::Instance().TryGetColorAndTags(path, &tagColor, &resolvedTags);
@@ -46,9 +65,10 @@ bool NameColorProvider::TryGetColorAndTags(const std::wstring& path, COLORREF* c
         }
     }
 
-    if (hasOverride) {
+    const ItemAppearance appearance = GetAppearanceForPath(path);
+    if (appearance.textColor.has_value()) {
         if (color) {
-            *color = overrideColor;
+            *color = *appearance.textColor;
         }
         return true;
     }
