22cac3eca8d225f43a324df79aabdd0836c885e7
shefben <shefben@gmail.com>
2025-10-19 21:22:47 -0500

    Hook folder view colorizer into BHO lifecycle

--- src/CExplorerBHO.cpp
+++ src/CExplorerBHO.cpp
@@ -207,9 +207,11 @@ IFACEMETHODIMP CExplorerBHO::SetSite(IUnknown* site) {
             if (!siteProvider && m_shellBrowser) {
                 m_shellBrowser.As(&siteProvider);
             }
-			// Colorize the active folder view
-			//m_colorizer.Attach(m_shellBrowser);
-			//m_colorizer.Refresh();
+
+            if (m_shellBrowser) {
+                m_colorizer.Attach(m_shellBrowser);
+                m_colorizer.Refresh();
+            }
 
 
             EnsureBandVisible();
@@ -338,6 +340,10 @@ IFACEMETHODIMP CExplorerBHO::Invoke(DISPID dispIdMember, REFIID, LCID, WORD, DIS
                     if (!m_bandVisible) {
                         EnsureBandVisible();
                     }
+                    break;
+                case DISPID_DOCUMENTCOMPLETE:
+                case DISPID_NAVIGATECOMPLETE2:
+                    m_colorizer.Refresh();
                     break;
                 case DISPID_ONQUIT:
                     Disconnect();
