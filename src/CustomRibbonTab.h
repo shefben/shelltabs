8ab65c08877e0a7c9122e3a0d9b7ca22fc307a75
Ben Shefte <shefben@gmail.com>
2025-11-26 16:07:04 -0600

    added CustomRibbonTab.bml and related files.

---
+++ src/CustomRibbonTab.h
@@ -0,0 +1,51 @@
+// *****************************************************************************
+// * This is an automatically generated header file for UI Element definition  *
+// * resource symbols and values. Please do not modify manually.               *
+// *****************************************************************************
+
+#pragma once
+
+#define cmdApplicationMenu 49990 
+#define cmdApplicationMenu_LabelTitle_RESID 60001
+#define cmdQuickAccessToolbar 49991 
+#define cmdQuickAccessToolbar_LabelTitle_RESID 60002
+#define cmdCustomTab 50000 
+#define cmdCustomTab_LabelTitle_RESID 60003
+#define cmdCustomTab_LabelDescription_RESID 60004
+#define cmdCustomGroup1 50001 
+#define cmdCustomGroup1_LabelTitle_RESID 60005
+#define cmdCustomButton1 50100 
+#define cmdCustomButton1_LabelTitle_RESID 60006
+#define cmdCustomButton1_LabelDescription_RESID 60007
+#define cmdCustomButton1_TooltipTitle_RESID 60008
+#define cmdCustomButton1_TooltipDescription_RESID 60009
+#define cmdCustomButton1_SmallImages_RESID 60010
+#define cmdCustomButton1_LargeImages_RESID 60011
+#define cmdCustomButton2 50101 
+#define cmdCustomButton2_LabelTitle_RESID 60012
+#define cmdCustomButton2_LabelDescription_RESID 60013
+#define cmdCustomButton2_TooltipTitle_RESID 60014
+#define cmdCustomButton2_TooltipDescription_RESID 60015
+#define cmdCustomButton2_SmallImages_RESID 60016
+#define cmdCustomButton2_LargeImages_RESID 60017
+#define cmdCustomButton3 50102 
+#define cmdCustomButton3_LabelTitle_RESID 60018
+#define cmdCustomButton3_LabelDescription_RESID 60019
+#define cmdCustomButton3_TooltipTitle_RESID 60020
+#define cmdCustomButton3_TooltipDescription_RESID 60021
+#define cmdCustomButton3_SmallImages_RESID 60022
+#define cmdCustomButton3_LargeImages_RESID 60023
+#define cmdCustomButton4 50103 
+#define cmdCustomButton4_LabelTitle_RESID 60024
+#define cmdCustomButton4_LabelDescription_RESID 60025
+#define cmdCustomButton4_TooltipTitle_RESID 60026
+#define cmdCustomButton4_TooltipDescription_RESID 60027
+#define cmdCustomButton4_SmallImages_RESID 60028
+#define cmdCustomButton4_LargeImages_RESID 60029
+#define cmdCustomButton5 50104 
+#define cmdCustomButton5_LabelTitle_RESID 60030
+#define cmdCustomButton5_LabelDescription_RESID 60031
+#define cmdCustomButton5_TooltipTitle_RESID 60032
+#define cmdCustomButton5_TooltipDescription_RESID 60033
+#define cmdCustomButton5_SmallImages_RESID 60034
+#define cmdCustomButton5_LargeImages_RESID 60035
